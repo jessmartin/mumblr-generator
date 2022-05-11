ebec05baec13f53ab9582459ded8d361fefa9f83
Jess Martin <jessmartin@gmail.com>
2022-05-11 12:28:14 -0400

    Trying out js-ipfs-fetch - prolly not going this way

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import IPFS from 'ipfs';
+// const IPFS = require('ipfs');
+const makeIpfsFetch = require('ipfs-fetch');
+const fetch = await makeIpfsFetch({ IPFS });
+
+const response = await fetch(
+  'ipfs://bafybeid42tsayaxar7zjtt6cgp5prqxdripty4zcky6r5ekmm3qxi66daq'
+);
+const text = await response.text();
+
+console.log(text);
